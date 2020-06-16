03be9a2a651e48f6be572edb5b8d01eb8715833f
aramaldes876@gmail.com <aramaldes876@gmail.com>
2020-06-16 19:18:36 -0300

    Nova alteração

--- Teste_do_Git/Program.cs
+++ Teste_do_Git/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Teste_do_Git
 {
@@ -11,6 +7,7 @@ namespace Teste_do_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo!");
+            Console.WriteLine();
         }
     }
 }
