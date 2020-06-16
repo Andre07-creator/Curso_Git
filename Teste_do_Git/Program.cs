a04f78b29944b63319ad3af3a91719ffa8c3f7c5
aramaldes876@gmail.com <aramaldes876@gmail.com>
2020-06-16 20:34:19 -0300

    Acrescimo de bom dia

--- Teste_do_Git/Program.cs
+++ Teste_do_Git/Program.cs
@@ -7,6 +7,7 @@ namespace Teste_do_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo!");
+            Console.WriteLine("Bom dia!!!");
             Console.WriteLine();
         }
     }
